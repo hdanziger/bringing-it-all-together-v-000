77bf5a64666a0afafde4cb3b6b0f760b5114cf06
Hannah Danziger <hdanziger8@gmail.com>
2017-10-27 17:12:47 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -23,6 +23,13 @@ class Dog
    DB[:conn].execute(sql)
  end
 
+ def self.new_from_db(row)
+   id = row[0]
+   name = row[1]
+   breed = row[2]
+   self.new(id, name, breed)
+ end
+
 end
 
 #DB[:conn]
